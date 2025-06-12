c057aee64b76509e426590fc3b688cd600566bc1
Oleg Z <108886071+oleza034@users.noreply.github.com>
2025-06-12 09:33:13 +0300

    moving chk_valid_triangle to a static method in Triangle class

--- src/triangle.py
+++ src/triangle.py
@@ -2,20 +2,11 @@ from math import sqrt
 from src.figure import Figure, chk_positive
 
 
-def chk_valid_triangle(a: (int | float), b: (int | float), c: (int | float)):
-    """Checks if three side lengths can form a valid triangle."""
-    # first, check that all sides are positive numbers
-    [chk_positive(num) for num in [a, b, c]]
-    # then, validate triangle
-    if not ((a + b > c) and (a + c > b) and (b + c > a)):
-        raise ValueError(f'Cannot form a valid triangle with sides: {a}, {b}, {c}')
-
-
 class Triangle(Figure):
     def __init__(self, side_a, side_b, side_c):
         """Creates a triangle figure with side_a and side_b and side_c."""
         # checking that sides can form a valid triangle
-        chk_valid_triangle(side_a, side_b, side_c)
+        self.chk_valid_triangle(side_a, side_b, side_c)
         self.side_a, self.side_b, self.side_c = side_a, side_b, side_c
 
     @property
@@ -30,3 +21,12 @@ class Triangle(Figure):
     def perimeter(self):
         """Calculates the perimeter of the triangle."""
         return self.side_a + self.side_b + self.side_c
+
+    @staticmethod
+    def chk_valid_triangle(a: (int | float), b: (int | float), c: (int | float)):
+        """Checks if three side lengths can form a valid triangle."""
+        # first, check that all sides are positive numbers
+        [chk_positive(num) for num in [a, b, c]]
+        # then, validate triangle
+        if not ((a + b > c) and (a + c > b) and (b + c > a)):
+            raise ValueError(f'Cannot form a valid triangle with sides: {a}, {b}, {c}')
