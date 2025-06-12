4247bbc90fd2e5ec30ca9b498144d2bdec36e583
Oleg Z <108886071+oleza034@users.noreply.github.com>
2025-06-12 09:30:48 +0300

    Fix anti-pattern to for construction

--- src/rectangle.py
+++ src/rectangle.py
@@ -5,7 +5,8 @@ class Rectangle(Figure):
     def __init__(self, side_a, side_b):
         """Creates a rectangle figure with side_a and side_b."""
 
-        [chk_positive(side, f'{self.__class__.__name__} sides') for side in [side_a, side_b]]
+        for side in [side_a, side_b]:
+            chk_positive(side, f'{self.__class__.__name__} sides')
         self.side_a = side_a
         self.side_b = side_b
 
